12dcd07b9da5aa9f57a0de66fa8f32afb9bafeaa
greenMakaroni <mrtn.dawid@gmail.com>
2022-12-25 11:10:41 +0000

    setup navigation and create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+//  react-router-dom components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//  pages
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Portfolio from "./pages/portfolio/Portfolio"
+import Contact from "./pages/contact/Contact"
+
 function App() {
 
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route exact path="/about" element={ <About /> } />
+        <Route exact path="/portfolio" element={ <Portfolio /> } />
+        <Route exact path="/contact" element={ <Contact /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
