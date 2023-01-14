79b43833fb456e5853aedd6d1cd2b853d229f4bb
greenMakaroni <mrtn.dawid@gmail.com>
2023-01-14 21:20:28 +0000

    Add portal model to the canvas, add lights, install drei, add orbit controls helpers

--- src/components/landingCanvas/Landing.jsx
+++ src/components/landingCanvas/Landing.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Box from "./Box"
 import { Canvas, useFrame } from '@react-three/fiber'
+import Portal from "./Portal"
+import { OrbitControls } from '@react-three/drei';
 
 const Landing = () => {
   return (
     <Canvas>
-      <Box />
+      
+       <ambientLight intensity={ 0.6 } />
+
+        <pointLight
+          intensity={0.5}       
+          position={ [-25, 35, -12] } 
+        />           
+      <Portal />
+      <OrbitControls />
     </Canvas>
 
   )
