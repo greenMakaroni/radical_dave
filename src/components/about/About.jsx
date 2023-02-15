d93e3c19bfe254a8269940d2af987bf641739129
greenMakaroni <mrtn.dawid@gmail.com>
2023-02-15 22:19:07 +0000

    about page setup

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,33 @@ import "./about.scss"
 const About = () => {
     return (
         <div className="aboutSection">
-
+            <h1 className="about-header"> ABOUT </h1>
+            <div className="about">
+                <div className="about-element">
+                    <h1 className="about-title"> Title </h1>
+                    <p className="element-par"> Paragraph </p>
+                </div>
+                <div className="about-element">
+                    <h1 className="about-title"> Title </h1>
+                    <p className="element-par"> Paragraph </p>
+                </div>
+                <div className="about-element">
+                    <h1 className="about-title"> Title </h1>
+                    <p className="element-par"> Paragraph </p>
+                </div>
+                <div className="about-element">
+                    <h1 className="about-title"> Title </h1>
+                    <p className="element-par"> Paragraph </p>
+                </div>
+                <div className="about-element">
+                    <h1 className="about-title"> Title </h1>
+                    <p className="element-par"> Paragraph </p>
+                </div>
+                <div className="about-element">
+                    <h1 className="about-title"> Title </h1>
+                    <p className="element-par"> Paragraph </p>
+                </div>
+            </div>
         </div>
     )
 }
